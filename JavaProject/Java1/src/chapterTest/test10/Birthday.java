8a86213fa25aad6e23d732d4b73f7c8ca31ca17a
JJSbok <sprpsa33@naver.com>
2023-04-04 16:36:35 +0900

    0404

--- JavaProject/Java1/src/chapterTest/test10/Birthday.java
+++ JavaProject/Java1/src/chapterTest/test10/Birthday.java
@@ -7,16 +7,16 @@ public static void main(String[] args) {
 	Scanner sc =new Scanner(System.in);
 	System.out.println("태어난 년도를 입력하세요 : ");
 
-	String d = sc.next();
-	sc.close();
-    try {
-        int intD = Integer.parseInt(d);
-        System.out.println(intD);
+	try {
+    int d = sc.nextInt();
+    System.out.println("태어난 년도 : " +d);
+    sc.close();
     } catch (Exception e) {
         System.out.println("숫자를 입력해야 합니다.");
+        
        return ;
     }
-    System.out.println("태어난 년도 : " +d);
+    
     
 }
 }
